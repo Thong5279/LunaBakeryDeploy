65f47faf4354a9fc5eefe5dfec1234d95f6aafad
PhamHuynhThong <numuanuocnoi@gmail.com>
2025-06-07 01:15:17 +0700

    flex

--- frontend/src/pages/OderconfirmationPage.jsx
+++ frontend/src/pages/OderconfirmationPage.jsx
@@ -47,7 +47,7 @@ const OderconfirmationPage = () => {
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white">
       <h1 className="text-4xl font-bold text-center text-emerald-700 mb-8">
-        Xác nhận đơn hàng thành công
+        Xác nhận đơn hàng thành công!
       </h1>
 
       {checkout && (
