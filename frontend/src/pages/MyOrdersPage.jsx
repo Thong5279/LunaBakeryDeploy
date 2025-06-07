2b8a9b741418213d90e742e91ed533bf0a1eafc2
PhamHuynhThong <numuanuocnoi@gmail.com>
2025-06-08 06:13:54 +0700

    fix2

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const MyOrdersPage = () => {
   const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const mockOrders = [
@@ -37,9 +39,12 @@ const MyOrdersPage = () => {
     const timer = setTimeout(() => {
       setOrders(mockOrders);
     }, 1000);
-    
-    return () => clearTimeout(timer); // Cleanup timer khi component unmount
+
   }, []);
+
+  const handleRowClick = (orderId) => {
+    navigate(`/order/${orderId}`); // Chuyển hướng đến trang chi tiết đơn hàng
+  };
 
   return (
     <div className='max-w-7xl mx-auto p-4 sm:p-6'>
@@ -61,7 +66,10 @@ const MyOrdersPage = () => {
           <tbody>
             {orders.length > 0 ? (
               orders.map((order) => (
-                <tr key={order._id} className='border-b hover:bg-gray-50 cursor-pointer'>
+                <tr 
+                key={order._id} 
+                onClick={() => handleRowClick(order._id)} // Chuyển hướng đến trang chi tiết đơn hàng
+                className='border-b hover:bg-gray-50 cursor-pointer'>
                   <td className='py-2 px-2 sm:py-4 sm:px-4'>
                     <img src={order.orderItems[0].image} alt={order.orderItems[0].name} className='w-10 h-10 sm:h-12 sm:w-12 object-cover rounded' />
                   </td>
