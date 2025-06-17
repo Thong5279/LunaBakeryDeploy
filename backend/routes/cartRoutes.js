5a25976c3764a5d8cfa2c36a6b7990a027d9a630
PhamHuynhThong <numuanuocnoi@gmail.com>
2025-06-17 18:53:05 +0700

    Cart Route Update

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,94 +1,134 @@
-const express = require('express');
-const Cart = require('../models/Cart');
-const Product = require('../models/Product');
-const { protect } = require('../middleware/authMiddleware');
+const express = require("express");
+const Cart = require("../models/Cart");
+const Product = require("../models/Product");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 //helper function to get cart by userId or guestId . hàm trợ giúp để lấy giỏ hàng theo userId hoặc guestId
 const getCart = async (userId, guestId) => {
-    if (userId) {
-        // If user is logged in, find cart by user ID
-        return await Cart.findOne({ user: userId });
-    } else if (guestId) {
-        // If guest, find cart by guest ID
-        return await Cart.findOne({ guestId });
-    } else {
-        // If neither, return null
-        return null;
-    }
-}
+  if (userId) {
+    // If user is logged in, find cart by user ID
+    return await Cart.findOne({ user: userId });
+  } else if (guestId) {
+    // If guest, find cart by guest ID
+    return await Cart.findOne({ guestId });
+  } else {
+    // If neither, return null
+    return null;
+  }
+};
 
 // @route POST /api/cart
 // @desc Add product to cart for a guest or logged-in user  .  Thêm sản phẩm vào giỏ hàng cho khách hoặc người dùng đã đăng nhập
-// @access Public 
+// @access Public
 
-router.post('/', async (req, res) => {
-    const { productId, quantity, size, flavor ,guestId ,userId } = req.body;
+router.post("/", async (req, res) => {
+  const { productId, quantity, size, flavor, guestId, userId } = req.body;
 
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    //determine if the user is logged in or a guest
+    let cart = await getCart(userId, guestId);
+
+    //if the cart exists, update it
+    if (cart) {
+      const productIndex = cart.products.findIndex(
+        (e) =>
+          e.productId.toString() === productId.toString() &&
+          e.size === size &&
+          e.flavor === flavor
+      );
+
+      if (productIndex > -1) {
+        // If product already exists in cart, update quantity
+        cart.products[productIndex].quantity += quantity;
+      } else {
+        // If product does not exist, add it to cart
+        cart.products.push({
+          productId: product._id,
+          name: product.name,
+          image: product.image,
+          price: product.price,
+          quantity,
+          size,
+          flavor,
+        });
+      }
+      // Recalculate total price
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      const newCart = await Cart.create({
+        user: userId ? userId : undefined,
+        guestId: guestId ? guestId : "guest_" + new Date().getTime(), // Generate a unique guest ID
+        products: [
+          {
+            productId,
+            name: product.name,
+            image: product.images[0].url, // Assuming the first image is the main one
+            price: product.price,
+            quantity,
+            size,
+            flavor,
+          },
+        ],
+        totalPrice: product.price * quantity,
+      });
+      return res.status(201).json(newCart);
+    }
+  } catch (error) {
+    console.error("Error adding product to cart:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//@route GET /api/cart
+//@desc update product quantity in cart for a logged-in user or guest
+// @access Private
+router.put("/", async (req,res ) => {
+    const { productId, quantity, size, flavor, guestId, userId } = req.body;
+    
     try {
-        const product = await Product.findById(productId); 
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found' });
+        const cart = await getCart(userId, guestId);
+        if (!cart) {
+        return res.status(404).json({ message: "Cart not found" });
         }
-
-        //determine if the user is logged in or a guest
-        let cart = await getCart(userId,guestId);
-
-        //if the cart exists, update it
-        if(cart){
-            const productIndex = cart.products.findIndex(
-                (e) =>
-                 e.productId.toString() === productId.toString() &&
-                    e.size === size &&
-                    e.flavor === flavor
-            );
-
-            if (productIndex > -1) {
-                // If product already exists in cart, update quantity
-                cart.products[productIndex].quantity += quantity;
+    
+        const productIndex = cart.products.findIndex(
+        (e) =>
+            e.productId.toString() === productId &&
+            e.size === size &&
+            e.flavor === flavor
+        );
+    
+        if (productIndex > -1) {
+            if( quantity >0) {
+                // If product exists in cart, update quantity
+                cart.products[productIndex].quantity = quantity;
             } else {
-                // If product does not exist, add it to cart
-                cart.products.push({
-                    productId: product._id,
-                    name: product.name,
-                    image: product.image,
-                    price: product.price,
-                    quantity,
-                    size,
-                    flavor
-                });
+                cart.products.splice(productIndex, 1); // Remove product if quantity is 0
             }
-            // Recalculate total price
             cart.totalPrice = cart.products.reduce(
-                (acc , item) => acc + item.price * item.quantity,
-             0
+                (acc, item) => acc + item.price * item.quantity,
+                0
             );
             await cart.save();
             return res.status(200).json(cart);
-        }else{
-            const newCart = await Cart.create({
-                user: userId ? userId : undefined, 
-                guestId: guestId ? guestId : "guest_" + new Date().getTime(), // Generate a unique guest ID
-                products: [
-                    {
-                    productId,
-                    name: product.name,
-                    image: product.images[0].url, // Assuming the first image is the main one
-                    price: product.price,
-                    quantity,
-                    size,
-                    flavor,
-                }
-            ],
-            totalPrice: product.price * quantity,
-            });
-            return res.status(201).json(newCart);
+        } else {
+        return res.status(404).json({ message: "Product not found in cart" });
         }
     } catch (error) {
-        console.error('Error adding product to cart:', error);
-        res.status(500).json({ message: 'Server error' });
-        
+        console.error("Error updating product quantity:", error);
+        res.status(500).json({ message: "Server error" });
     }
 })
 
